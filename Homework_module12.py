8ac6884d452c09c01bc12a40709e0c56e099c1c2
Syr010 <yana.syrtseva@yandex.ru>
2022-05-06 16:42:59 +0200

    new file

---
+++ Homework_module12.py
@@ -0,0 +1,14 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму: "))
+deposit = []
+for value in per_cent.values():
+   result = int(money / 100 * value)
+   deposit.append(result)
+print(deposit)
+print(max(deposit))
+
+
+
+
+
+
